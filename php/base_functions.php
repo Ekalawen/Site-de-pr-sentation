5ff8db28be5bb3a0abdf2810d0c4bb62811974ed
Ekalawen <alexys.dussier@gmail.com>
2017-10-14 23:40:33 +0200

    la structure est prête gs

---
+++ php/base_functions.php
@@ -0,0 +1,137 @@
+<?php
+
+	function getHeader($actif, $entete = true, $nav = true) {
+		$acActif = ($actif == "acceuil") ? 'class="onglet_actif"' : '';
+		$apActif = ($actif == "apropos") ? 'class="onglet_actif"' : '';
+		$prActif = ($actif == "projets") ? 'class="onglet_actif"' : '';
+		$jpActif = ($actif == "jeux") ? 'class="onglet_actif"' : '';
+		$ctActif = ($actif == "contact") ? 'class="onglet_actif"' : '';
+
+		$entete = $entete ? '
+			<ul>
+				<li>
+					<button>FR</button>
+				</li>
+				<li>
+					<button>ANG</button>
+				</li>
+			</ul>
+			<div>
+				<h1>
+					Alexys Dussier
+				</h1>
+				<h3>
+					Engineering Student, Aspiring Game Designer
+				</h3>
+			</div>
+		' : '';
+
+		$sousMenuProjets = '';
+		/*
+						<ul class="sous_menu">
+							<li>
+								<a href="sss.php">Speed Skill Style</a>
+							</li>
+							<li>
+								<a href="ir2.php">Icon Rush 2</a>
+							</li>
+							<li>
+								<a href="sss.php">Clicker</a>
+							</li>
+						</ul>
+		*/
+
+		$nav = $nav ? '
+			<nav>
+				<ul>
+					<li ' . $acActif . '>
+						<a href="acceuil.php">Accueil</a>
+					</li>
+					<li ' . $apActif . '>
+						<a href="a_propos.php">À propos</a>
+					</li>
+					<li ' . $prActif . '>
+						<a href="projets.php">Projets</a>
+						' . $sousMenuProjets . '
+					</li>
+					<li ' . $jpActif . '>
+						<a href="jeux_preferes.php">Jeux préférés</a>
+					</li>
+					<li ' . $ctActif . '>
+						<a href="contact.php">Contact</a>
+					</li>
+				</ul>
+			</nav>
+		' :  '';
+
+		$script = '
+		<script type="text/javascript">
+$(document).ready( function () {
+     $(".sous_menu").hide();    
+
+     $("span").each( function () {
+     $(this).replaceWith(\'<a href="" title="Afficher le sous-menu">\' + $(this).text() + \'<\/a>\') ;
+     } ) ;
+
+	 $(".sous_menu > a").click( function () {
+	 if ($(this).next(".sous_menu:visible").length != 0) {
+	     $(this).next(".sous_menu").slideUp("normal");
+	 }
+	 else {
+	     $(".sous_menu").slideUp("normal");
+	     $(this).next(".sous_menu").slideDown("normal");
+	 }
+	 return false;
+	 });    
+} ) ;
+</script>
+		';
+
+		$header = '
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Alexys Dussier</title>
+		<meta charset="UTF-8">
+		<meta name="description" content="Page d\'Acceuil">
+		<meta name="keywords" content="Alexys, Dussier, CV, jeux, game, design">
+		<meta name="author" content="Alexys Dussier">
+		<!--<meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+		<style type="style.css"></style>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="reset.css">
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+		<!--<link rel="stylesheet" type="text/css" href="visionneuse.css"> -->
+	</head>
+
+	<body>
+		<header>
+			' . $entete . '
+			' . $nav . '
+		</header>
+		';
+
+		return $header;
+	}
+
+	function getFooter() {
+
+		$footer = '
+		<footer>
+		</footer>
+	</body>
+</html>
+		';
+
+		return $footer;
+	}
+
+	function getCV() {
+
+		$cv = '
+			<iframe src="images/cv/Alexys_Dussier_CV_FR.pdf" width="100%" height="100%" align="middle"></iframe>
+		';
+
+		return $cv;
+	}
+?>
---
+++ projets.php
@@ -0,0 +1,8 @@
+<?php
+	require_once("php/base_functions.php");
+
+	echo getHeader("projets");
+
+	echo getFooter();
+
+ ?>
