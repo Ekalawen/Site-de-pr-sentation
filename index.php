894205cceb4cf4a46bc1a1988b121c8ba617f402
Elystria <petitevague@hotmail.fr>
2017-10-16 22:59:34 +0200

    prise de notes

--- index.php
+++ index.php
@@ -7,6 +7,47 @@
 
 	echo getFooter();
 
+	function getText1() {
+		$text = '
+			Alexys Dussier
+			Je suis ingénieur en formation à l\'N7 et ma plus grande passion, c\'est le Game Design !
+			Créer des jeux est mon passe temps depuis que je suis tout petit ! D\'ailleurs vous en trouverez une partie dans la section "Mes Projets" !
+		';
+
+		return $text;
+	}
+
+	function getPunchLine() {
+		return '
+			"A good idea is something that does not solve just one problem, but rather can solve multiple problems at once.” Shigeru Miyamoto
+		';
+	}
+
+	function getMeCaractériserEn3Mots () {
+		return '
+			Esprit Critique
+			Innovant et Créatif
+			Autonome
+			Rigoureux
+
+			Théoricien (des jeux)
+			Programmeur (de jeux)
+			Joueur (try hard)
+		';
+	}
+
+	function getCoupsDeCoeurs () {
+		return '
+			SC2 (le jeu le plus difficile à maîtriser du monde, tout en ayant du sens)
+			Ori and the blind Forest (une immersion émmotionnelle totale, un design du déplacement époustouflant)
+			Magicka (un algorithme de combinaison infinie de possibilités intégré au jeu !)
+			Dofus (mmo qui met l\'accent sur la connaissance plutôt que sur le temps de jeu)
+			Speluncky (un des meilleurs usages de la génération aléatoire de cartes ! et j\'adore les RogueLikes !)
+			SuperHexagon (Un principe simple et des moyens limités pour créer quelque chose de différent + super hard core !)
+			Coockie Clicker (un jeu qui assume d\'abuser du concept de la boîte de Skinner jusqu\'au bout !)
+		';
+	}
+
 
 	function getTextAcceuil() {
 		$text = '
