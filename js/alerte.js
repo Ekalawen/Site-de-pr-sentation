d8da81412067c38bec4fd3d4b5be7638fb75e310
Ekalawen <alexys.dussier@gmail.com>
2017-10-23 21:53:22 +0200

    ajout de l'alerte js de démarrage

---
+++ js/alerte.js
@@ -0,0 +1,6 @@
+
+
+// Alerte de démarrage
+jQuery(document).ready(function(){
+ alert("Dear Visitor, my website is currently under construction.\nDo not hesitate to contact me.\nalexys.dussier@gmail.com");
+});
