82bada58d2ce36f831b5e0c7f901265df282b2e4
Ekalawen <alexys.dussier@gmail.com>
2017-10-24 22:14:20 +0200

    Sauver Mathieu gs <3

--- js/alerte.js
+++ js/alerte.js
@@ -1,6 +1,8 @@
 
 
 // Alerte de démarrage
-jQuery(document).ready(function(){
- alert("Dear Visitor, my website is currently under construction.\nDo not hesitate to contact me.\nalexys.dussier@gmail.com");
-});
+function alerteDemarrage() {
+	jQuery(document).ready(function(){
+	 alert("Dear Visitor, my website is currently under construction.\nDo not hesitate to contact me.\nalexys.dussier@gmail.com");
+	});
+}
