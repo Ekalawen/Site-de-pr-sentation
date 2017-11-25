aa2559d336c6fe5dc85a9b423ac3c8fdf9cce037
Ekalawen <alexys.dussier@gmail.com>
2017-11-25 21:14:44 +0100

    suppression du stock d'images + ajout des boutons dans About me

--- index.php
+++ index.php
@@ -11,30 +11,28 @@
 	function getTextAcceuil() {
 		$text = '
 		<section class="theme4">
-			<div class="ligne 66">
+			<div class="ligne">
 				<p>
 					 Dear Visitor, my website is currently under construction.
 					 Thank you for your understanding.
 				</p>
 			</div>
 		</section>
-		<section class="theme1">
-			<div class="ligne w50">
-				<div class="colonne">
-					<h3>
-						Who I am ?
-					</h3>
-					<p>
-						I am an IT engineering student passionate about designing and creating games. My dream is to create the next generation gaming experience.
-					</p>
-					<img id="photo_profile" src="images/moi/photo_profile.jpg" alt="Vous ne me voyez pas ?" />
-					<p id="text_photo_profile" class="centerText">
-					</p>
-				</div>
+		<section class="theme1 colonne">
+				<h3>
+					Who I am ?
+				</h3>
+			<div class="w50">
+				<p>
+					I am an IT engineering student passionate about designing and creating games. My dream is to create the next generation gaming experience.
+				</p>
 			</div>
+			<img id="photo_profile" src="images/moi/photo_profile.jpg" alt="Vous ne me voyez pas ?" />
+			<p id="text_photo_profile" class="centerText">
+			</p>
 		</section>
 		<a href="a_propos.php">
-			<section class="theme3" id="redirection_about_me">
+			<section class="theme3 colonne" id="redirection_about_me">
 				<div class="ligne w50">
 						<p id="redirection_about_me_p">
 							Do you want to know more about me ?
