bd884c2215249f0db78e906c280b68001e3355fb
Ekalawen <alexys.dussier@gmail.com>
2019-03-04 13:54:34 -0500

    correction :i ==> I

--- projet_ir2.php
+++ projet_ir2.php
@@ -16,7 +16,7 @@
 					Icon Rush 2 !
 				</h1>
 				<h3>
-					This is the first video game i created.
+					This is the first video game I created.
 				</h3>
 				<h3>
 					It will, thanks to this, always be my favorite one.
@@ -33,7 +33,7 @@
 					Part one : A color racing (rushing ?) game !
 				</h3>
 				<p class="justifyText">
-					In Icon Rush 2 you are playing a square who represent an Icon. The Icons are usually emblematics : Google, Linux, StarCraft 2, Magic and so on. Yes i will have issues with copyright. No this game is not for sale, don\'t worry.<br> But you are not alone ! You can play up to 6 person on the same computer ! It\'s a local game, and it unleashes the Fun with friends !<br>
+					In Icon Rush 2 you are playing a square who represent an Icon. The Icons are usually emblematics : Google, Linux, StarCraft 2, Magic and so on. Yes I will have issues with copyright. No this game is not for sale, don\'t worry.<br> But you are not alone ! You can play up to 6 person on the same computer ! It\'s a local game, and it unleashes the Fun with friends !<br>
 					Soooo, what is the goal ? You must convert the field to your own color. You achieve this by just moving. And by moving you create bonuses. Quickly catch them before your opponents in order to take the advantage. <br>
 					And that\'s all ! =)
 				</p>
@@ -58,8 +58,8 @@
 					Part two : I love squares !
 				</h3>
 				<p class="justifyText">
-					I made this game when i was in middle School. Then i worked on it again in preparatory School (that\'s why "2").<br>
-					And this time, i wanted to make it looks awesome ! But i am not a graphist. I am a programmer. So i thought to a way to look beautiful without any pictures, and I say "I master the squares, i will create fireworks with them !".<br>
+					I made this game when I was in middle School. Then I worked on it again in preparatory School (that\'s why "2").<br>
+					And this time, I wanted to make it looks awesome ! But I am not a graphist. I am a programmer. So I thought to a way to look beautiful without any pictures, and I say "I master the squares, I will create fireworks with them !".<br>
 					<br>
 					Oh ! And did I say it\'s random ? Each time you go to this screen, the squares will follow a different pattern.
 				</p>
