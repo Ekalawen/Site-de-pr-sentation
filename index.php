7c7673d34ee0d90ad363b3fca35cfaee1a4c0f32
Ekalawen <alexys.dussier@gmail.com>
2019-03-04 14:03:58 -0500

    ajout des 10 pages que je vais remplir

--- index.php
+++ index.php
@@ -26,16 +26,107 @@
 			</p>
 		</section>
 		<section class="theme2">
-			<a href="projet_ir2.php">
-				<div class="size300-200" id="projet_img_ir2">
+
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
 					<div class="ligne w100 h100">
 						<p class="volatileText">
-							Icon Rush 2
+							Battle for Pangora
 						</p>
 					</div>
 				</div>
 			</a>
-
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							AI Bot Camp
+						</p>
+					</div>
+				</div>
+			</a>
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							HadokeMDP
+						</p>
+					</div>
+				</div>
+			</a>
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							ParanoCrisis
+						</p>
+					</div>
+				</div>
+			</a>
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							Le Prince des Voleurs
+						</p>
+					</div>
+				</div>
+			</a>
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							Digital World
+						</p>
+					</div>
+				</div>
+			</a>
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							La Fièvre du Sommet
+						</p>
+					</div>
+				</div>
+			</a>
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							Find the Bear
+						</p>
+					</div>
+				</div>
+			</a>
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							GameJam 2018
+						</p>
+					</div>
+				</div>
+			</a>
+			
+			<a href="index.php">
+				<div class="size300-200" id="projet_img1">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							Immersive Factory ?
+						</p>
+					</div>
+				</div>
+			</a>
+			
 			<a href="projet_sss.php">
 				<div class="size300-200" id="projet_img_sss">
 					<div class="ligne w100 h100">
@@ -56,11 +147,11 @@
 				</div>
 			</a>
 
-			<a href="projet_clicker.php">
-				<div class="size300-200" id="projet_img_clicker">
+			<a href="projet_ir2.php">
+				<div class="size300-200" id="projet_img_ir2">
 					<div class="ligne w100 h100">
 						<p class="volatileText">
-							Pixel Clicker
+							Icon Rush 2
 						</p>
 					</div>
 				</div>
@@ -76,6 +167,17 @@
 				</div>
 			</a>
 			
+
+			<a href="projet_clicker.php">
+				<div class="size300-200" id="projet_img_clicker">
+					<div class="ligne w100 h100">
+						<p class="volatileText">
+							Pixel Clicker
+						</p>
+					</div>
+				</div>
+			</a>
+
 			<a href="projet_bus.php">
 				<div class="size300-200" id="projet_img_bus">
 					<div class="ligne w100 h100">
@@ -85,6 +187,7 @@
 					</div>
 				</div>
 			</a>
+
 			<a href="index.php">
 				<div class="size300-200" id="projet_img1">
 					<div class="ligne w100 h100">
@@ -94,6 +197,7 @@
 					</div>
 				</div>
 			</a>
+
 			<a href="index.php">
 				<div class="size300-200" id="projet_img1">
 					<div class="ligne w100 h100">
