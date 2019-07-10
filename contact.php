d137eb109dabafe35315ec3c5ebe65524fc174b5
Ekalawen <alexys.dussier@gmail.com>
2019-07-10 14:13:22 +0200

    contact form responsive

--- contact.php
+++ contact.php
@@ -16,12 +16,12 @@
 			<section class="theme2 colonne">
 				<form class="colonne" method="post" name="myemailform" action="php/sendMail.php">
 					<h3>Enter Name *</h3>
-					<input type="text" name="name">
+					<input type="text" class="resp_small_extend" name="name">
 					<h3>Enter Email Address *</h3>
-					<input type="text" name="email">
+					<input type="text" class="resp_small_extend" name="email">
 					<h3>Enter Message *</h3>
-					<textarea class="size500 verticalSize250" name="message"></textarea>
-					<input class="activable" type="submit" value="Send Form !">
+					<textarea class="size500 verticalSize250 resp_extend" name="message"></textarea>
+					<input class="activable resp_small_extend" type="submit" value="Send Form !">
 				</form>
 			</section>
 		</section>
